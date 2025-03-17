471240465d964bce71c4826d986a760615e271f1
Tao <tao.sun@mail.mcgill.ca>
2025-03-17 13:35:43 -0400

    move_for_rebar_project

---
+++ rebar_move_demo.py
@@ -0,0 +1,42 @@
+import zmq
+import time
+import numpy as np
+from base_server import BaseManager
+from constants import BASE_RPC_HOST, BASE_RPC_PORT, RPC_AUTHKEY
+from constants import POLICY_CONTROL_PERIOD
+
+def main():
+    # set up a ZMQ context and a REP socket
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    # Bind to an address/port that is accessible from local PC at port 6666
+    socket.bind("tcp://*:6666")
+
+    manager = BaseManager(address=(BASE_RPC_HOST, BASE_RPC_PORT), authkey=RPC_AUTHKEY)
+    manager.connect()
+    base = manager.Base(max_vel=(0.5, 0.5, 1.57), max_accel=(0.25, 0.25, 0.79))
+    base.reset()
+    print("Base is reset and ready. Listening for commands via ZMQ...")
+
+    try:
+        while True:
+            # 1. Wait for next request from client
+            message = socket.recv_pyobj()  
+            
+            # 2. Parse the message. Example: { "action": [0.0, 0.0, 1.57] }
+            action = message.get("action", None)
+            # 3. Execute action if provided
+            if action is not None:
+                base.execute_action({'base_pose': np.array(action)})
+                
+                print("Executed action:", action)
+            robot_state = socket.send_pyobj(base.get_state())
+            print(base.get_state())
+            time.sleep(POLICY_CONTROL_PERIOD) # Note: Not precise
+    
+    finally:
+        base.close()
+        print("Base movement over.")
+
+if __name__ == "__main__":
+    main()
---
+++ rebar_target_move.py
@@ -0,0 +1,16 @@
+import zmq
+import numpy as np
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+
+# Suppose the remote server has IP 192.168.1.123, port 6666
+socket.connect("tcp://10.50.245.100:6666")
+
+for i in range(100):
+    # Default: abs target pose, local frame
+    rep = {'action': np.array([0.0, 1.88, 0.0])}  # i / 100 * 1.05
+     # min(0.2, i / 50) * 3.14   0* 3.14
+    socket.send_pyobj(rep)
+    reply = socket.recv_pyobj() 
+    # print("Received reply:", reply)
